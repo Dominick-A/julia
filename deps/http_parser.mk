94c1328fba384c558d7e0963857affb10bd2da05
Art <wildart@gmail.com>
2016-06-30 11:08:06 -0400

    windows config

--- deps/http_parser.mk
+++ deps/http_parser.mk
@@ -8,15 +8,21 @@ HTTPPARSER_OBJ_TARGET := $(build_shlibdir)/libhttp_parser.$(SHLIB_EXT)
 HTTPPARSER_OBJ_SOURCE := $(BUILDDIR)/$(HTTPPARSER_SRC_DIR)/libhttp_parser.$(SHLIB_EXT)
 
 $(HTTPPARSER_OBJ_SOURCE): $(BUILDDIR)/$(HTTPPARSER_SRC_DIR)/Makefile
+	cd $(dir $@) && patch < $(SRCDIR)/patches/http_parser.patch
 	$(MAKE) -C $(dir $<) library $(MAKE_COMMON)
 	touch -c $@
+
 $(HTTPPARSER_OBJ_TARGET): $(HTTPPARSER_OBJ_SOURCE)
 	$(call make-install,$(HTTPPARSER_SRC_DIR), library PREFIX=$(build_staging)/$(HTTPPARSER_SRC_DIR)$(build_prefix))
+ifeq ($(OS), WINNT)
+	mv -f $(build_libdir)/libhttp_parser.$(SHLIB_EXT) $@
+endif
 	touch -c $(HTTPPARSER_OBJ_TARGET)
 
 clean-http_parser:
 	-rm -rf $(BUILDDIR)/$(HTTPPARSER_SRC_DIR)
 	-rm -f $(HTTPPARSER_OBJ_TARGET)
+	-rm -f $(build_libdir)/libhttp_parser.*
 
 get-http_parser: $(HTTPPARSER_SRC_FILE)
 configure-http_parser: $(BUILDDIR)/$(HTTPPARSER_SRC_DIR)/Makefile
--- deps/mbedtls.mk
+++ deps/mbedtls.mk
@@ -4,13 +4,25 @@ MBEDTLS_GIT_URL := git://github.com/ARMmbed/mbedtls.git
 MBEDTLS_TAR_URL = https://api.github.com/repos/ARMmbed/mbedtls/tarball/$1
 $(eval $(call git-external,mbedtls,MBEDTLS,CMakeLists.txt,build/libmbedtls.$(SHLIB_EXT),$(SRCDIR)/srccache))
 
-MBEDTLS_OBJ_SOURCE := $(BUILDDIR)/$(MBEDTLS_SRC_DIR)/library/libmbedtls.$(SHLIB_EXT) $(BUILDDIR)/$(MBEDTLS_SRC_DIR)/library/libmbedx509.$(SHLIB_EXT) $(BUILDDIR)/$(MBEDTLS_SRC_DIR)/library/libmbedcrypto.$(SHLIB_EXT)
-MBEDTLS_OBJ_TARGET := $(build_shlibdir)/libmbedtls.$(SHLIB_EXT) $(build_shlibdir)/libmbedx509.$(SHLIB_EXT) $(build_shlibdir)/libmbedcrypto.$(SHLIB_EXT)
+MBEDTLS_OBJ_SOURCE := $(BUILDDIR)/$(MBEDTLS_SRC_DIR)/library/libmbedtls.$(SHLIB_EXT) \
+			$(BUILDDIR)/$(MBEDTLS_SRC_DIR)/library/libmbedx509.$(SHLIB_EXT) \
+			$(BUILDDIR)/$(MBEDTLS_SRC_DIR)/library/libmbedcrypto.$(SHLIB_EXT)
+MBEDTLS_OBJ_TARGET := $(build_shlibdir)/libmbedtls.$(SHLIB_EXT) \
+			$(build_shlibdir)/libmbedx509.$(SHLIB_EXT) \
+			$(build_shlibdir)/libmbedcrypto.$(SHLIB_EXT)
 
-MBEDTLS_OPTS := $(CMAKE_COMMON) -DUSE_SHARED_MBEDTLS_LIBRARY=ON -DENABLE_PROGRAMS=OFF -DENABLE_ZLIB_SUPPORT=ON -DENABLE_TESTING=OFF -DCMAKE_INSTALL_RPATH=$(build_prefix) -DCMAKE_INSTALL_RPATH_USE_LINK_PATH=TRUE -DCMAKE_BUILD_TYPE=RelWithDebInfo
+MBEDTLS_OPTS := $(CMAKE_COMMON) -DUSE_SHARED_MBEDTLS_LIBRARY=ON \
+		-DENABLE_PROGRAMS=OFF -DENABLE_TESTING=OFF \
+		-DCMAKE_BUILD_TYPE=RelWithDebInfo
+
+ifeq ($(OS), WINNT)
+MBEDTLS_OPTS += -G"MSYS Makefiles" -DENABLE_ZLIB_SUPPORT=OFF
+else
+MBEDTLS_OPTS += -DENABLE_ZLIB_SUPPORT=ON -DCMAKE_INSTALL_RPATH=$(build_prefix) -DCMAKE_INSTALL_RPATH_USE_LINK_PATH=TRUE
+endif
 
 $(BUILDDIR)/$(MBEDTLS_SRC_DIR)/Makefile: $(SRCDIR)/srccache/$(MBEDTLS_SRC_DIR)/CMakeLists.txt
-	-cd $(SRCDIR)/srccache/$(MBEDTLS_SRC_DIR) && patch -p0 -f < $(SRCDIR)/mbedtls.patch
+	-cd $(SRCDIR)/srccache/$(MBEDTLS_SRC_DIR) && patch -p0 -f < $(SRCDIR)/patches/mbedtls.patch
 	mkdir -p $(dir $@)
 	cd $(dir $@) && \
 	$(CMAKE) $(dir $<) $(MBEDTLS_OPTS)
@@ -27,7 +39,11 @@ endif
 	echo 1 > $@
 
 $(MBEDTLS_OBJ_TARGET): $(MBEDTLS_OBJ_SOURCE) | $(build_shlibdir)
+ifeq ($(OS), WINNT)
+	cp $^ $(build_shlibdir)
+else
 	$(call make-install,$(MBEDTLS_SRC_DIR),)
+endif
 	touch -c $(MBEDTLS_OBJ_TARGET)
 
 clean-mbedtls:
